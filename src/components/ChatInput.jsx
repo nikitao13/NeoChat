571c2a2116cfa21f7e5c46211601d6b50ca22c4f
nikitao13 <okeeffe.nikita@gmail.com>
2024-01-26 13:54:56 +1100

    moar CSS changes

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,8 +1,14 @@
 function ChatInput() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("message sent");
+    }
+
+
     return (
         <div id="chat-input">
             <div id="input-bar">
-                <form id="form">
+                <form id="form" onSubmit={handleSubmit}>
                     <input type="text" id="message-input" placeholder="type a message..." />
                     <button type="submit" id="send-button">send</button>
                 </form>
